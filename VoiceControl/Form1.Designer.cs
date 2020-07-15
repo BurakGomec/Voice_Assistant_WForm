28e40a3889caa8ae9f40e2bc63c261075a34d45b
BURAK GÖMEÇ <48028551+BurakGomec@users.noreply.github.com>
2020-07-15 18:14:00 +0300

    starting project

---
+++ VoiceControl/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace VoiceControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(178, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(750, 250);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(428, 352);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(323, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Personal Voice Assistant";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(1082, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VoiceControl/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;//Sesi Tanıma
+using System.Speech.Synthesis;
+using System.Media;
+using System.Diagnostics;
+using System.Collections;
+using System.Globalization;//totitlecase
+
+namespace VoiceControl
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+
+        //SpeechSynthesizer syn = new SpeechSynthesizer();
+        SoundPlayer player = new SoundPlayer();
+        string on =Application.StartupPath+"\\SiriOn.wav";
+        string off =Application.StartupPath+ "\\SiriOff.wav";
+        string understood =Application.StartupPath+"\\SiriUnderstood.wav";
+        
+        private void StartingVoice()
+        {
+            try
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                Choices choices = new Choices();
+                string[] words = { "hello", "paint", "word", "google", "youtube", "what time is it","how are you"
+                ,"hey assistant","open google","exit the application"};
+                choices.Add(words);
+                Grammar grammar = new Grammar(new GrammarBuilder(choices));
+                //Grammar grammar = new DictationGrammar();
+                rec.LoadGrammar(grammar);
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+                rec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Speechrecognized);
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Lutfen bilgisayarinizin dilini ingilizceye ceviriniz", "Error!");
+                Application.Exit();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString(),"Error!");
+            }
+
+
+       
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Speechrecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //timer1.Enabled = true;
+            
+            string result = e.Result.Text;
+            string newy = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(result);
+            richTextBox1.AppendText("You: " + newy + Environment.NewLine);
+            if (result == "Hey assistant")
+            {
+                result = "Hi,how can i help u ";
+            }
+            if (result == "hello")
+            {
+                result = "Hello,How are you";
+
+            }
+            if(result== "what time is it" )
+            {
+                result = "It is" + DateTime.Now.ToLongTimeString();
+            }
+            if (result.Contains("youtube"))
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/?gl=TR");
+                result = "Opening youtube";
+            }
+           
+            if (result.Contains("google"))
+            {
+                result = "Opening google";
+                System.Diagnostics.Process.Start("https://www.google.com.tr");
+            }
+            if (result == "open paint")
+            {
+                result = "It is" + DateTime.Now.ToLongTimeString();
+            }
+            if (result == "open word")
+            {
+                result = "It is" + DateTime.Now.ToLongTimeString();
+            }
+            if (result == "exit the application")
+            {
+                result = "Okey closing it";
+                Application.Exit();
+            }
+            if (result == "what time is it")
+            {
+                result = "It is" + DateTime.Now.ToLongTimeString();
+            }
+            if (result == "what time is it")
+            {
+                result = "It is" + DateTime.Now.ToLongTimeString();
+            }
+            else if(result == "" || result==null)
+            {
+                result = "Would you repeat?";
+            }
+
+            player.SoundLocation = understood;
+            player.Play();
+            speech.SpeakAsync(result);
+            richTextBox1.AppendText("Assistant: " + result + Environment.NewLine);
+            // System.Diagnostics.Process.Start("MSpaint.Exe");
+            //rec.Dispose();
+            //speech.Dispose();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            player.SoundLocation = on;
+            player.Play();
+            StartingVoice();
+            pictureBox1.Enabled = false;
+        }
+
+    }
+}
